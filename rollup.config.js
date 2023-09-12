bd3e94b7b8700d1ccabff00c41240bab6b9d6a2c
pycd <chenwx@gdhmail.com>
2023-09-12 18:04:11 +0800

    添加rollup打包

---
+++ rollup.config.js
@@ -0,0 +1,14 @@
+import typescript from "@rollup/plugin-typescript";
+import commonjs from "@rollup/plugin-commonjs";
+import terser from "@rollup/plugin-terser";
+
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      file: "dist/index.js",
+      format: "cjs",
+    }
+  ],
+  plugins: [typescript(), commonjs(), terser()]
+};
